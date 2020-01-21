59d7f1701f2e51473dcf284d4eb52b8addcc8f9c
s_arme <s.arme@ya.ru>
2020-01-21 11:44:23 +0330

    Add job model

---
+++ Models/Job.py
@@ -0,0 +1,19 @@
+from Models.Models import db
+from datetime import datetime
+
+
+class Job(db.Model):
+    __tablename__ = 'job'
+    jid = db.Column(db.String(100), primary_key=True)
+    result = db.Column(db.Text(), nullable=True)
+    created = db.Column(db.DateTime())
+    description = db.Column(db.Text(300), nullable=False)
+
+    def __init__(self,jid: str, result: str, description: str):
+        self.jid = jid
+        self.result = result
+        self.description = description
+        self.created = datetime.now()
+
+    def __repr__(self):
+        return "< {0} {1} {2}>".format(self.jid, self.description, self.created.strftime('%b %d, %Y'))
